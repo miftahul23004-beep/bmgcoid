fed07489d5bb10f59035548059814edef051907d
miftahul23004-beep <miftahul.23004@mhs.unesa.ac.id>
2026-02-03 08:42:31 +0700

    Fix: Remove font preload causing 404 errors

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -47,14 +47,8 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     
-    {{-- Preload critical fonts for LCP optimization --}}
-    <link rel="preload" href="https://fonts.gstatic.com/s/inter/v13/UcCO3FwrK3iLTeHuS_fvQtMwCp50KnMw2boKoduKmMEVuLyfAZ9hjp-Ek-_EeA.woff2" as="font" type="font/woff2" crossorigin>
-    
     {{-- Fonts with display=swap for FOUT optimization --}}
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Plus+Jakarta+Sans:wght@600;700&display=swap" rel="stylesheet" media="print" onload="this.media='all'">
-    <noscript>
-        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Plus+Jakarta+Sans:wght@600;700&display=swap" rel="stylesheet">
-    </noscript>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Plus+Jakarta+Sans:wght@600;700&display=swap" rel="stylesheet">
 
     {{-- Critical CSS for preventing CLS --}}
     <style>
